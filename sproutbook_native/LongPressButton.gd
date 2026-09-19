8235e56bcf6faf698fdbc37a0349c77cc921ca43
nothatcher-creator <nothatcher@gmail.com>
2026-09-19 13:16:28 -0300

    v201: optimize native rendering and tree interactions

--- sproutbook_native/LongPressButton.gd
+++ sproutbook_native/LongPressButton.gd
@@ -1,7 +1,7 @@
 extends Button
 signal long_pressed
 
-@export var hold_seconds := 0.45
+@export var hold_seconds := 0.48
 var held := false
 var fired := false
 var started_at := 0
@@ -9,18 +9,21 @@ var started_at := 0
 func _ready() -> void:
     button_down.connect(_down)
     button_up.connect(_up)
-    set_process(true)
+    set_process(false)
 
 func _down() -> void:
     held = true
     fired = false
     started_at = Time.get_ticks_msec()
+    set_process(true)
 
 func _up() -> void:
     held = false
+    set_process(false)
 
 func _process(_delta: float) -> void:
     if held and not fired and Time.get_ticks_msec() - started_at >= int(hold_seconds * 1000.0):
         fired = true
         held = false
+        set_process(false)
         long_pressed.emit()
