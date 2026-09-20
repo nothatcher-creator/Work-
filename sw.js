8625d09acd2ad01bef6d1861d6b42ce4062d61dc
nothatcher-creator <nothatcher@gmail.com>
2026-09-20 09:20:36 -0300

    Refresh BlendWeb v0.3 cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='blendweb-v2';
+const CACHE='blendweb-v3';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
